b2ad9e01275a204afac63ccdd37e66503c4ace43
Aman Inamdar <79567211+amaninamdar09@users.noreply.github.com>
2022-03-18 21:34:55 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -15,7 +15,7 @@ async def _(event):
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     input_str = event.pattern_match.group(1)
     if tgbotusername is not None or mafia_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@MafiaBot_Support")
+        results = await event.client.inline_query(tgbotusername, "@BLACKPANTHERSBOLTE")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
@@ -40,7 +40,7 @@ async def info(event):
     input_str = event.pattern_match.group(1)
     if input_str == "text":
         string = (
-            "Total {count} commands found in {plugincount} sudo plugins of MafiaBot\n\n"
+            "Total {count} commands found in {plugincount} sudo plugins of 𝙿𝙰𝙽𝚃𝙷𝙴𝚁𝚂 𝙱𝙾𝚃 \n\n"
         )
         mafiacount = 0
         plugincount = 0
